1d707408cccc9e7bda352a4ad6e6af471e62da02
halkity <saito75061@gmail.com>
2020-07-02 08:19:35 +0900

    Implemented member registration after incremental search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,19 +1,29 @@
 $(function() {
-  let chatMemberList = $("#UserSearchResult");
+  let chatMemberAddList = $("#UserSearchResult");
+  let chatMemberRemoveList = $(".ChatMembers")
   function addUser(user) {
     let html = 
     `<div class="ChatMember clearfix">
       <p class="ChatMember__name">${user.name}</p>
       <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
     </div>`;
-  chatMemberList.append(html);
+  chatMemberAddList.append(html);
   }
   function addNoUser() {
     let html = 
     `<div class="ChatMember clearfix">
       <p class="ChatMember__name">ユーザーが見つかりません</p>
     </div>`;
-    chatMemberList.append(html);
+    chatMemberAddList.append(html);
+  }
+  function addMember(name, id) {
+    let html = 
+      `<div class="ChatMember">
+        <p class="ChatMember__name">${name}</p>
+        <input name="group[user_ids][]" type="hidden" value="${id}" />
+        <div class="ChatMember__remove ChatMember__button">削除</div>
+      </div>`
+    chatMemberRemoveList.append(html);
   }
   $('#UserSearch__field').on("keyup", function () {
     let input = $('#UserSearch__field').val();
@@ -25,7 +35,7 @@ $(function() {
     })
     .done(function (users) {
       //emptyメソッドで一度検索結果を空にする
-      chatMemberList.empty();
+      chatMemberAddList.empty();
       //usersが空かどうかで条件分岐
       if (users.length !== 0) {
         //配列オブジェクト１つ１つに対する処理
@@ -40,6 +50,17 @@ $(function() {
     })
     .fail(function () {
       alert("通信エラーです。ユーザーが表示できません。");
+    });
+    // 追加ボタンが押された時にイベントが発火
+    chatMemberAddList.on("click", ".ChatMember__add", function () {
+      console.log(this);
+      const userName = $(this).attr("data-user-name");
+      const userId = $(this).attr("data-user-id");
+      $(this).parent().remove();
+      addMember(userName, userId);
+    })
+    chatMemberRemoveList.on("click", ".ChatMember__remove", function () {
+      $(this).parent().remove();
     });
   })
 })

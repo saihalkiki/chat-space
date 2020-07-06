88cf57cb6182cad5b1a92bd04bbfabae4cc8c104
halkity <saito75061@gmail.com>
2020-07-06 16:39:13 +0900

    add sumple user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "sample",
+             email: "sample@sample.com",
+             password:              "samplesample",
+             password_confirmation: "samplesample")
+
+# 追加のユーザーをまとめて生成する
+99.times do |i|
+  name  = Faker::Name.initials(number: 6)
+  email = "example-#{i+1}@example.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
